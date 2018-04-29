87761d2803a095e97bfb338549bd9b37cb97de64
wangleiR <1220632559@qq.com>
2018-04-29 23:12:04 +0800

    completed the test

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -31,8 +31,8 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //6.将*的名字改成*,输出这次修改影响的行数
     @Modifying
     @Query("update Employee e set e.name = ?2 where e.name = ?1")
-    @Transactional
-    Integer updateEmployeeName(String name,String newName);
+
+    Integer updateName(String name,String newName);
 
     //7.删除姓名是*的employee
     void deleteByName(String name);
